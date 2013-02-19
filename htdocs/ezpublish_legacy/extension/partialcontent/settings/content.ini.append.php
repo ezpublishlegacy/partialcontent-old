3bc5f84c95bd2bc7145e9fc2af751d6fbd2bc49a
Joe Kepley <joe@blendinteractive.com>
2013-02-19 11:42:51 -0600

    Added some missing syntax types

--- htdocs/ezpublish_legacy/extension/partialcontent/settings/content.ini.append.php
+++ htdocs/ezpublish_legacy/extension/partialcontent/settings/content.ini.append.php
@@ -12,11 +12,16 @@ AvailableClasses[]=code_php
 AvailableClasses[]=code_javascript
 AvailableClasses[]=code_twig
 AvailableClasses[]=code_xml
+AvailableClasses[]=code_shell
+AvailableClasses[]=code_yml
 
 ClassDescription[html]=Raw HTML Output
+ClassDescription[code_text]=Syntax Highlights - Plain text
+ClassDescription[code_shell]=Syntax Highlights - Shell script
 ClassDescription[code_html]=Syntax Highlights - HTML
 ClassDescription[code_php]=Syntax Highlights - PHP
 ClassDescription[code_css]=Syntax Highlights - CSS
 ClassDescription[code_javascript]=Syntax Highlights - Javascript
 ClassDescription[code_twig]=Syntax Highlights - Twig
 ClassDescription[code_xml]=Syntax Highlights - XML
+ClassDescription[code_yml]=Syntax Highlights - YAML
